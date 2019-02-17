223ddd1f213eb812b7d2483adadfe65a8a701bad
Michael Verhulst <michael@terminallabs.com>
2019-02-16 23:02:14 -0600

    Update system-package.sh

--- system-package.sh
+++ system-package.sh
@@ -1,10 +1,10 @@
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
 
-sudo apt -y install python3
-sudo apt -y install python3-pip
-sudo apt -y install python3-dev
-sudo apt -y install python3-venv
+apt -y install python3
+apt -y install python3-pip
+apt -y install python3-dev
+apt -y install python3-venv
 
-sudo apt -y install libjpeg-dev zlib1g-dev
-sudo apt -y install emacs 
+apt -y install libjpeg-dev zlib1g-dev
+apt -y install emacs 
