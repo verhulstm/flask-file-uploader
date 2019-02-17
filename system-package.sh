ca6ff94b3f9ec393309d25f4e16ebb716f3e607f
Michael Verhulst <michael@terminallabs.com>
2019-02-16 22:41:11 -0600

    Update system-package.sh

--- system-package.sh
+++ system-package.sh
@@ -5,5 +5,5 @@ sudo apt -y install python3
 sudo apt -y install python3-pip
 sudo apt -y install python3-dev
 
-sudo apt -y install libjpeg-dev
+sudo apt -y install libjpeg-dev zlib1g-dev
 sudo apt -y install emacs 
