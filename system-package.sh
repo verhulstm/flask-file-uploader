e6790b39cf13faef59150b8d86c53da4d6637ad1
Michael Verhulst <michael@terminallabs.com>
2019-02-16 23:00:20 -0600

    added in python3-venv

--- system-package.sh
+++ system-package.sh
@@ -4,6 +4,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confd
 sudo apt -y install python3
 sudo apt -y install python3-pip
 sudo apt -y install python3-dev
+sudo apt -y install python3-venv
 
 sudo apt -y install libjpeg-dev zlib1g-dev
 sudo apt -y install emacs 
